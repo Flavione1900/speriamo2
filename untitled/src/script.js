d5f4787537f0632be639b6c4fc145f8d66a1b6c2
Flavio <flavio190071@gmail.com>
2024-12-10 16:53:05 +0100

    Add files via upload

---
+++ untitled/src/script.js
@@ -0,0 +1,27 @@
+function scopri() {
+  window.location.href = 'https://armundia.com/';
+}
+
+function leggi() {
+  window.location.href = 'https://drive.google.com/file/d/1mq7LzMBCeYG4DjTw_Fz3tel6qYO9Y0em/view?usp=sharing';
+}
+
+function guarda() {
+  window.location.href = 'https://youtu.be/Yz7rb0_YNP8';
+}
+
+function invia() {
+  window.location.href = 'mailto:recruitment@armundiagroup.com';
+}
+
+function linkedin() {
+  window.location.href = 'https://www.linkedin.com/company/armundia-group/';
+}
+
+function instagram() {
+  window.location.href = 'https://www.instagram.com/armundia_group?igsh=eG03ZWZsYW5ob3h5';
+}
+
+function youtube() {
+  window.location.href = 'https://www.youtube.com/@armundiagroup9701';
+}
